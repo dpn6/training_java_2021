3bea73866c48a91642ae8115b60c06e14d6b563e
dmitrieva polina <dpn6@ya.ru>
2021-07-15 10:32:46 +0700

    Создан класс Point и MySecondTask. Реализована функция для вычисления расстояния между двумя точками

---
+++ sandbox/src/main/java/ru/stqa/dmiv/sandbox/MySecondTask.java
@@ -0,0 +1,16 @@
+package ru.stqa.dmiv.sandbox;
+
+public class MySecondTask {
+
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+  }
+
+  public static void main(String [] args){
+    Point p1 = new Point(7, 11);
+    Point p2 = new Point(4, 7);
+
+    System.out.println("Расстояние между двумя точкам с координатами первой точки x: " + p1.x + ", y: " + p1.y
+    + " и координатами второй точки x: "  + p2.x + ", y: " + p2.y + " равняется " + distance(p1, p2) );
+  }
+}
