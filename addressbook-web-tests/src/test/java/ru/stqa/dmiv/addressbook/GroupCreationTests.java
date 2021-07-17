541ab6098c324e3ffcef7dab6ee854a37997941e
dmitrieva polina <dpn6@ya.ru>
2021-07-18 00:09:37 +0700

    Из теста выделена часть кода в блок инициализации фикстуры

--- addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.dmiv.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,13 +10,9 @@ public class GroupCreationTests {
   private WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void setUp(){
     wd = new FirefoxDriver();
-       wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("https://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -23,6 +20,10 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.id("LoginForm")).submit();
+  }
+
+  @Test
+  public void testGroupCreation(){
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
@@ -35,13 +36,12 @@ public class GroupCreationTests {
     wd.findElement(By.name("group_footer")).sendKeys("test3");
     wd.findElement(By.name("submit")).click();
     wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
+  public void tearDown(){
     wd.quit();
-     }
+  }
 
   private boolean isElementPresent(By by) {
     try {
