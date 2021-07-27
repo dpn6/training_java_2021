29c823a7b5ca9c946a55d3346ec7999d402817b9
dmitrieva polina <dpn6@ya.ru>
2021-07-27 16:28:54 +0700

    Реализована возможность запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/TestBase.java
@@ -1,12 +1,14 @@
 package ru.stqa.dmiv.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.dmiv.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
+  protected String browser = BrowserType.FIREFOX;
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(browser);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() {
