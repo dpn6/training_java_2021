1b3cefac88425b8b0ec9d6dbb69a840afc14c359
dmitrieva polina <dpn6@ya.ru>
2021-08-09 08:16:19 +0700

    Для тестов по работе с группами предусловие вынесено в BeforeMethod

--- addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.dmiv.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.dmiv.addressbook.model.GroupData;
 
@@ -8,12 +9,17 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
 
-  @Test
-  public void testGroupDeletionTests(){
+  @BeforeMethod
+  void precondition(){
     app.getNavigationHelper().gotoGroupPage();
     if (!app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
+  }
+
+  @Test
+  public void testGroupDeletionTests(){
+
     List<GroupData> before =  app.getGroupHelper().getGroupList();
     int lastIdx = before.size()-1;
     app.getGroupHelper().selectGroup(lastIdx);
--- addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.dmiv.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.dmiv.addressbook.model.GroupData;
 
@@ -9,12 +10,16 @@ import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
-  @Test
-  public void testGroupModification() {
+  @BeforeMethod
+  void precondition() {
     app.getNavigationHelper().gotoGroupPage();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", null));
     }
+  }
+
+  @Test
+  public void testGroupModification() {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     int lastIdx = before.size() - 1;
     int id = before.get(lastIdx).getId();
