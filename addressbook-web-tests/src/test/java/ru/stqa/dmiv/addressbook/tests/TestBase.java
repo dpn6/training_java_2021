ddfd549b451b87611aac1c79fb0474c47e77bba9
dmitrieva polina <dpn6@ya.ru>
2021-08-22 19:03:18 +0700

    Протоколирование с помощью logger

--- addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/TestBase.java
@@ -1,18 +1,25 @@
 package ru.stqa.dmiv.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.dmiv.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
-  @BeforeSuite(alwaysRun = true)
+  @BeforeSuite
   public void setUp() throws IOException {
     app.init();
   }
@@ -20,6 +27,16 @@ public class TestBase {
   @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("stop test " + m.getName());
   }
 
 }
