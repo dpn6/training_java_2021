1bccaf7431590b37e6b741a27382bb3499ef008d
dmitrieva polina <dpn6@ya.ru>
2021-08-26 07:48:58 +0700

    Добавлена проверка списков из UI-интерфейса для Модификации группы

--- addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/dmiv/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.stqa.dmiv.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,10 +10,17 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.dmiv.addressbook.appmanager.ApplicationManager;
+import ru.stqa.dmiv.addressbook.model.GroupData;
+import ru.stqa.dmiv.addressbook.model.Groups;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -36,8 +45,21 @@ public class TestBase {
   }
 
   @AfterMethod(alwaysRun = true)
-  public void logTestStop(Method m){
+  public void logTestStop(Method m) {
     logger.info("stop test " + m.getName());
   }
 
+
+  public void verifyGroupListInUi() {
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups uiGroups = app.group().all();
+      Groups dbGroups = app.db().groups();
+//    Set<GroupData> groupDataSet = app.db().groups().stream().map(g -> new GroupData().withId(g.getId()).withName(g.getName())).collect(Collectors.toSet());
+//    Groups dbGroups = new Groups(groupDataSet);
+//    assertThat(uiGroups, equalTo(dbGroups));
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map(g -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
+  }
 }
