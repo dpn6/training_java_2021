0de31528605984fbce3f675379b448b110c37c4f
dmitrieva polina <dpn6@ya.ru>
2021-09-06 08:17:17 +0700

    Написан тест для регистрации пользователя в Mantis

--- mantis-tests/src/test/java/ru/stqa/dmiv/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/dmiv/mantis/tests/RegistrationTests.java
@@ -21,9 +21,10 @@ public class RegistrationTests extends TestBase {
 
   @Test
   public void testRegistration() throws IOException {
-    String email = "user2@localhost.localdomain";
-    String user = "user2";
-    String password = "password2";
+    long now = System.currentTimeMillis();
+    String user = "user" + now;
+    String password = "password" + now;
+    String email = String.format("user%s@localhost.localdomain", now);
     app.registration().start(user, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
